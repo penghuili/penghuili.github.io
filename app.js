e6132a0ae473d084ce56ad149d6cad0c7abe857e
penghui li <ichpenghui@gmail.com>
2017-05-07 23:09:19 +0200

    add remove

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 function show18(data) {
+  let el = document.querySelector(".ph-button--18");
   if (data.country_code !== "DE") {
-    document.querySelector(".ph-button--18").style.display = "inline-block";
+    el.style.display = "inline-block";
+  } else {
+    let parent = document.querySelector(".ph-mobile");
+    parent.removeChild(el);
   }
 }
