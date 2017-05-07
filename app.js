08a9d45d24c6d65eb1917536c930519bb25de32b
penghui li <ichpenghui@gmail.com>
2017-05-07 23:04:14 +0200

    test again

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function show18(data) {
-  if (data.country_code !== "CN") {
+  if (data.country_code !== "DE") {
     document.querySelector(".ph-button--18").style.display = "inline-block";
   }
 }
