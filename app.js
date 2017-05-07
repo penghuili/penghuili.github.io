6fdddc823fbe83bcc30b17e602c48bea1dd92f98
penghui li <ichpenghui@gmail.com>
2017-05-07 21:56:14 +0200

    try jsonp

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-window.onload = show18();
-
-function show18() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function(data) {
-    if (this.readyState == 4 && this.status == 200) {
-     console.log(data)
-    }
-  };
-  xhttp.open("GET", "freegeoip.net/json/", true);
-  xhttp.send();
+function show18(data) {
+  console.log(data);
 }
