bad45a0edf2a169d4b2886ffccde23574f8b925d
penghui li <ichpenghui@gmail.com>
2017-05-07 23:10:43 +0200

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function show18(data) {
   let el = document.querySelector(".ph-button--18");
-  if (data.country_code !== "DE") {
+  if (data.country_code !== "CN") {
     el.style.display = "inline-block";
   } else {
     let parent = document.querySelector(".ph-mobile");
