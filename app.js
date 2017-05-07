24915babd939db81afd0db1cff6f9df50b048508
penghui li <ichpenghui@gmail.com>
2017-05-07 22:56:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ function show18(data) {
   console.log(data);
 }
 
-$(document).ready(function() {
-  $.getJSON('https://freegeoip.net/json/?callback?', show18);
-});
+// $(document).ready(function() {
+//   $.getJSON('https://freegeoip.net/json/?callback?', show18);
+// });
