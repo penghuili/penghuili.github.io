6772177ea1a1f2201f82eb48418432451095df92
penghui li <ichpenghui@gmail.com>
2017-06-15 10:40:57 +0200

    delete 18

--- app.js
+++
@@ -1,9 +0,0 @@
-function show18(data) {
-  let el = document.querySelector(".ph-button--18");
-  if (data.country_code !== "CN") {
-    el.style.display = "inline-block";
-  } else {
-    let parent = document.querySelector(".ph-mobile");
-    parent.removeChild(el);
-  }
-}
